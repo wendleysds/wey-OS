30f28698d0b08100206d2bc77a7a34702d4e3998
Wendley Santos <wendley.sdsilva@gmail.com>
2025-02-25 18:13:09 -0300

    feat: hello world kernel

---
+++ src/kernel/main.c
@@ -0,0 +1,28 @@
+#define VGA_MEMORY (volatile char*)0xB8000
+
+void clear_screen() {
+	volatile char* videoMemory = VGA_MEMORY;
+	for(int i = 0; i < 80 * 25 * 2; i++){
+		videoMemory[i * 2] = ' ';
+		videoMemory[i * 2 + 1] = 0x0F;
+	}
+}
+
+void vga_write(const char* message, unsigned char color) {
+	volatile char* videoMemory = VGA_MEMORY;
+	int i = 0;
+
+	while(message[i] != '\0'){
+		videoMemory[i * 2] = message[i];
+		videoMemory[i * 2 + 1] = color;
+		i++;
+	}
+}
+
+void kmain(void* muitiboot_structure, unsigned int magicnumber) {
+	clear_screen();
+	vga_write("Hello World", 0x0F);
+	while (1);
+}
+
+
