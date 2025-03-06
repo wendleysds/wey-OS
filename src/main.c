c212845a978a125261092f7275fa2e47b55f397b
Wendley Santos <wendley.sdsilva@gmail.com>
2025-03-06 00:36:41 -0300

    chore: terminal default color macro

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "terminal/terminal.h"
+#include "kernel/kernel.h"
 
 void kmain(){
 	terminal_clear();
 	terminal_write("Hello, World!\n\n", 0x0F);
 	terminal_write("Welcome to my OS! :D\n", 0x0F);
+	
+	init_kernel();
 
 	while(1);
 }
--- src/terminal/terminal.h
+++ src/terminal/terminal.h
@@ -1,6 +1,8 @@
 #ifndef _TERMINAL_H
 #define _TERMINAL_H
 
+#define TERMINAL_DEFAULT_COLOR 0x0F
+
 void terminal_clear();
 void terminal_write(const char* message, unsigned char color);
 
