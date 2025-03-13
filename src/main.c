350923ee4377306d12a989ce89a3ed246ac4a25f
Wendley Santos <wendley.sdsilva@gmail.com>
2025-03-13 03:17:08 -0300

    fix: terminal default color and new message

--- src/main.c
+++ src/main.c
@@ -6,8 +6,9 @@ void kmain(){
 	terminal_cursor_disable();
 	
 	terminal_clear();
-	terminal_write("Hello, World!\n\n", 0x0F);
-	terminal_write("Welcome to my OS! :D\n", 0x0F);
+	terminal_write("Hello, World!\n", TERMINAL_DEFAULT_COLOR);
+	terminal_write("Welcome to my OS! :D\n", TERMINAL_DEFAULT_COLOR);
+	terminal_write("\n--------------------------\n", TERMINAL_DEFAULT_COLOR);
 	
 	init_kernel();
 
