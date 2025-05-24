5b6fff20833a03427171a967d438cc806852b745
Wendley Santos <wendley.sdsilva@gmail.com>
2025-05-24 20:00:10 -0300

    feat(terminal): implementation and macros
    - Adapted to use the new Font struct
    - Create RGB converters to support more VBE modes
      - RGB888 to RGB565
      - RGB888 to RGB555
    - Implemented Backspace
    - Set static private funciontons

--- src/drivers/video/terminal.c
+++ src/drivers/video/terminal.c
@@ -15,25 +15,39 @@
 
 #define BACKGROUND_COLOR 0x0
 
+#define RGB888_TO_RGB565(r, g, b) \
+	(uint16_t)( ((r & 0b11111000) << 8) | ((g & 0b11111100) << 3) | ((b) >> 3) )
+
+#define RGB888_TO_RGB555(r, g, b) \
+	(uint16_t)( ((r >> 3) << 10) | ((g >> 3) << 5) | (b >> 3) )
+
 static struct Cursor cursor;
 static struct VideoStructPtr video;
+static struct FontInfo font;
 
 void vesa_putpixel(struct VideoStructPtr* v, uint16_t x, uint16_t y, uint32_t color){
 	uint32_t offset = y * v->pitch + x * (v->bpp / 8);
 	uint8_t* framebuffer = (uint8_t*)v->framebuffer_virtual;
 
+	uint8_t r,g,b;
+	r = (uint8_t)((color >> 16) & 0xFF);
+	g = (uint8_t)((color >> 8) & 0xFF);
+	b = (uint8_t)(color & 0xFF);
+		
 	switch (v->bpp) {
-		case 8:
+		case 8: 
 			framebuffer[offset] = (uint8_t)color;
 			break;
 		case 15: // RGB555
+			*(uint16_t*)(framebuffer + offset) = RGB888_TO_RGB555(r, g, b);
+			break;
     case 16: // RGB565
-			*(uint16_t*)(framebuffer + offset) = (uint16_t)color;
+			*(uint16_t*)(framebuffer + offset) = RGB888_TO_RGB565(r, g, b);
 			break;
 		case 24:
-			framebuffer[offset + 0] = (uint8_t)(color & 0xFF);
-			framebuffer[offset + 1] = (uint8_t)((color >> 8) & 0xFF);
-			framebuffer[offset + 2] = (uint8_t)((color >> 16) & 0xFF);
+			framebuffer[offset + 0] = r;
+			framebuffer[offset + 1] = g;
+			framebuffer[offset + 2] = b;
 			break;
 		case 32:
 			*(uint32_t*)(framebuffer + offset) = color;
@@ -44,22 +58,22 @@ void vesa_putpixel(struct VideoStructPtr* v, uint16_t x, uint16_t y, uint32_t co
 	}
 }
 
-static void vesa_printchar(const unsigned char* font, const char c, uint8_t fontWidth, uint8_t fontHeigth){
-	uint16_t x = cursor.x * fontWidth;
-	uint16_t y = cursor.y * fontHeigth;
+static void vesa_printchar(struct FontInfo* font, const char c, uint32_t color){
+	uint16_t x = cursor.x * font->width;
+	uint16_t y = cursor.y * font->heigth;
 
-	for (uint8_t column = 0; column < fontHeigth; column++){
+	for (uint8_t column = 0; column < font->heigth; column++){
 		//unsigned char line = font[(int)c][column];
-		unsigned char bitmapLine = font[(int)c * fontHeigth + column];
-		for (uint8_t row = 0; row < fontWidth; row++){
+		unsigned char bitmapLine = font->ptr[(int)c * font->heigth + column];
+		for (uint8_t row = 0; row < font->width; row++){
 			unsigned char mask = 0x80 >> row;
 			if(bitmapLine & mask)
-				vesa_putpixel(&video, x, y, 0xFFFFFFFF);
+				vesa_putpixel(&video, x, y, color);
 
 			x++;
 		}
 
-		x = cursor.x * fontWidth;
+		x = cursor.x * font->width;
 		y++;
 	}
 }
@@ -77,6 +91,10 @@ void terminal_init(){
 	cursor.enabled = 1;
 
 	video.framebuffer_virtual = video.framebuffer_physical;
+
+	font.width = 8;
+	font.heigth = 16;
+	font.ptr = (const unsigned char*)font8x16;
 }
 
 void display_video_info(){
@@ -89,7 +107,7 @@ void display_video_info(){
 	terminal_write("--------------\n\n");
 }
 
-void scroll_terminal(){
+static void scroll_terminal(){
 	// TODO: Implement
 }
 
@@ -114,7 +132,7 @@ void terminal_putchar(char c, uint32_t color) {
 		terminal_backspace();
 	}
 	else{
-		vesa_printchar((const unsigned char *)font8x16, c, 8, 16);
+		vesa_printchar(&font, c, color);
 
 		cursor.x++;
     if (cursor.x >= (video.width / 8)) {
@@ -127,9 +145,24 @@ void terminal_putchar(char c, uint32_t color) {
 }
 
 void terminal_backspace(){
-	// TODO: Implement
-}
+	if(cursor.x == 0){
+		return;
+	}
 
+	cursor.x--;
+	uint16_t x = cursor.x * font.width;
+	uint16_t y = cursor.y * font.heigth;
+
+	for (uint8_t column = 0; column < font.heigth; column++){
+		for (uint8_t row = 0; row < font.width; row++){
+			vesa_putpixel(&video, x, y, BACKGROUND_COLOR);
+			x++;
+		}
+
+		x = cursor.x * 8;
+		y++;
+	}
+}
 
 void terminal_vwrite(uint32_t color, const char *format, va_list args) {
   char buffer[32];
