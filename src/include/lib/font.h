9801504695ba838af1cf697aa70fa9e2667c6327
Wendley Santos <wendley.sdsilva@gmail.com>
2025-05-24 19:59:33 -0300

    feat: font struct abstraction

--- src/include/lib/font.h
+++ src/include/lib/font.h
@@ -1,6 +1,14 @@
 #ifndef _FONT_H
 #define _FONT_H
 
+#include <stdint.h>
+
+struct FontInfo{
+	uint8_t width;
+	uint8_t heigth;
+	const unsigned char* ptr;
+}__attribute__((packed));
+
 extern unsigned char font8x16[][16];
 
 #endif
