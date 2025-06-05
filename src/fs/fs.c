1ab68ccce3de69485fe789ec1ccacac01da61c19
Wendley Santos <wendley.sdsilva@gmail.com>
2025-06-04 23:00:48 -0300

    feat: fs_init

---
+++ src/fs/fs.c
@@ -0,0 +1,22 @@
+#include <fs/fs.h>
+#include <fs/file.h>
+#include <fs/fat/fat32.h>
+#include <lib/mem.h>
+#include <core/kernel.h>
+#include <def/status.h>
+#include <def/config.h>
+#include <stdint.h>
+
+static struct FileDescriptor* fileDescriptors[FILE_DESCRIPTORS_MAX];
+
+void fs_init() {
+	memset(fileDescriptors, 0x0, sizeof(fileDescriptors));
+	struct FAT fat;
+
+	int status = FAT32_init(&fat); 
+	if(status != SUCCESS){
+		panic("Failed to initialize File System! STATUS %d", status);
+	}
+}
+
+
