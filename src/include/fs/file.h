22c940d1b7174bc0632737d9d35793aa45a36ba9
Wendley Santos <wendley.sdsilva@gmail.com>
2025-06-06 17:21:51 -0300

    refractor: removed Path struct
    because of the strtok, a path data struct is obsollet.

--- src/include/fs/file.h
+++ src/include/fs/file.h
@@ -2,15 +2,10 @@
 #define _FILE_H
 
 #include <def/config.h>
+#include <stdint.h>
 
 struct FileDescriptor {};
 struct Stat {};
-
-struct Path {
-	char* pathTokens[PATH_MAX];
-};
-
-void path_parse(struct Path* pathbuf, const char* path);
 
 #endif
 
