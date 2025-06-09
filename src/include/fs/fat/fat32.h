89a1cd5e36108d7320da951c69e8696c092c736b
Wendley Santos <wendley.sdsilva@gmail.com>
2025-06-09 02:27:23 -0300

    feat: fat arg to prevend open withount one

--- src/include/fs/fat/fat32.h
+++ src/include/fs/fat/fat32.h
@@ -7,8 +7,8 @@
 
 int FAT32_init(struct FAT* fat);
 
-struct FATFileDescriptor* FAT32_open(const char* pathname, uint8_t flags, uint8_t mode);
-int FAT32_stat(const char* pathname, struct Stat* statbuf);
+struct FATFileDescriptor* FAT32_open(struct FAT* fat, const char* pathname, uint8_t flags, uint8_t mode);
+int FAT32_stat(struct FAT* fat, const char* pathname, struct Stat* statbuf);
 int FAT32_seek(struct FATFileDescriptor* ffd, uint32_t offset, uint8_t whence);
 int FAT32_read(struct FATFileDescriptor* ffd, void* buffer, uint32_t count);
 int FAT32_write(struct FATFileDescriptor* ffd, const void* buffer, uint32_t size);
