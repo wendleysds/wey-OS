6958e4d43eaabdb32822f14f44aa63f5d6543368
Wendley Santos <wendley.sdsilva@gmail.com>
2025-07-24 00:17:27 -0300

    perf: switched module to bitwise
    fixed some security errors

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,6 +1,5 @@
-#include <memory/mm.h>
+#include <mmu.h>
 #include <core/kernel.h>
-#include <lib/mem.h>
 #include <def/status.h>
 #include <stdint.h>
 
@@ -8,44 +7,40 @@
  * Paging Controller and Loader
  */
 
+#define _MASK 0xFFFFF000
+
 #define _ADDRS_NOT_ALING(virt, phys) \
-	(((uintptr_t)virt % PAGING_PAGE_SIZE) || \
-	((uintptr_t)phys % PAGING_PAGE_SIZE))
+	(((uintptr_t)(virt) & (PAGING_PAGE_SIZE - 1)) || \
+	((uintptr_t)(phys) & (PAGING_PAGE_SIZE - 1)))
 
 extern void paging_load_directory(uint32_t* addr);
 static uint32_t* currentDirectoryAddr = 0x0;
 
-static int _paging_is_aligned(void* addr){
-	return ((uintptr_t)addr % PAGING_PAGE_SIZE) == 0;
-}
-
-static int _get_indexes(void* virtualAddr, uint32_t* outDirIndex, PagingTable* outTabIndex){
-	if(!_paging_is_aligned(virtualAddr)){
-		return BAD_ALIGNMENT;
-	}
-
-	*outDirIndex = ((uintptr_t)virtualAddr / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
-	*outTabIndex = ((uintptr_t)virtualAddr % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
-	return SUCCESS;
+static void _get_indexes(void* virtualAddr, uint32_t* outDirIndex, uint32_t* outTabIndex){
+	uintptr_t virt = (uintptr_t)virtualAddr;
+	*outDirIndex = virt >> 22;
+	*outTabIndex = (virt >> 12) & 0x3FF;
 }
 
 static uint32_t _get_page(struct PagingDirectory* directory, void* virt){
-	uint8_t status = SUCCESS;
-    uint32_t dirIndex = 0;
-    uint32_t tblIndex = 0;
-    if((status = _get_indexes(virt, &dirIndex, &tblIndex)) != SUCCESS){
-		return status;
-	}
+    uint32_t dirIndex, tblIndex;
+    _get_indexes(virt, &dirIndex, &tblIndex);
     
 	uint32_t entry = directory->entry[dirIndex];
-    uint32_t* table = (uint32_t*)(entry & 0xFFFF000);
+	if (!(entry & FPAGING_P)) return 0;
+
+    uint32_t* table = (uint32_t*)(entry & _MASK);
     return table[tblIndex];
 }
 
-static void* _get_physical_address(struct PagingDirectory* directory, void* virt){
-    void* lowVirt = (void*) paging_align_to_lower(virt);
-    void* difference = (void*)((uintptr_t) virt - (uintptr_t) lowVirt);
-    return (void*)((_get_page(directory, lowVirt) & 0xfffff000) + difference);
+void* paging_translate(struct PagingDirectory* directory, void* virt){
+    uintptr_t lowVirt = (uintptr_t) paging_align_to_lower(virt);
+    uintptr_t difference = (uintptr_t) virt - lowVirt;
+
+	uintptr_t pagePhysic = _get_page(directory, (void*)lowVirt) & _MASK;
+	if (pagePhysic == 0) return NULL;
+
+    return (void*)(pagePhysic + difference);
 }
 
 void* paging_align_address(void* ptr){
@@ -78,14 +73,16 @@ struct PagingDirectory* paging_new_directory(uint32_t tablesAmount, uint8_t flag
 
 	int offset = 0;
 
-  for (uint32_t i = 0; i < tablesAmount; i++)
-  {
+	for (uint32_t i = 0; i < tablesAmount; i++){
 		PagingTable* table = (PagingTable*)kcalloc(sizeof(PagingTable) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-
 		if(!table){
-			directory->entry = entry;
-			directory->tableCount = tablesAmount - (i + 1);
-			return directory;
+			for(uint32_t j = 0; j < i; j++){
+				kfree((void*)(entry[j] & _MASK));
+			}
+
+			kfree(entry);
+			kfree(directory);
+			return 0x0;
 		}
 
 		for (int j = 0; j < PAGING_TOTAL_ENTRIES_PER_TABLE; j++){
@@ -110,8 +107,11 @@ void paging_switch(struct PagingDirectory *directory){
 void paging_free_directory(struct PagingDirectory *directory){
 	for(size_t i = 0; i < directory->tableCount; i++){
 		PagingTable tableEntry = directory->entry[i];
-		PagingTable* table = (PagingTable*)(tableEntry & 0xFFFFF000);
-		kfree(table);
+
+		if(!tableEntry & FPAGING_P){
+			PagingTable* table = (PagingTable*)(tableEntry & _MASK);
+			kfree(table);
+		}
 	}
 
 	kfree(directory->entry);
@@ -123,14 +123,8 @@ int paging_map(struct PagingDirectory* directory, void* virtualAddr, void* physi
 		return BAD_ALIGNMENT;
 	}
 
-	if(!_paging_is_aligned(virtualAddr)){
-		return BAD_ALIGNMENT;
-	}
-
 	uint32_t dirIndex, tblIndex;
-	if(_get_indexes(virtualAddr, &dirIndex, &tblIndex) != SUCCESS){
-		return BAD_ALIGNMENT;
-	}
+	_get_indexes(virtualAddr, &dirIndex, &tblIndex);
 
 	if(!(directory->entry[dirIndex] & FPAGING_P)){
 		PagingTable* newTable = (PagingTable*)kcalloc(sizeof(PagingTable) * PAGING_TOTAL_ENTRIES_PER_TABLE);
@@ -142,7 +136,7 @@ int paging_map(struct PagingDirectory* directory, void* virtualAddr, void* physi
 		directory->entry[dirIndex] = (PagingTable)newTable | FPAGING_P | FPAGING_RW | FPAGING_US;
 	}
 
-	PagingTable *table = (PagingTable*)(directory->entry[dirIndex] & 0xFFFFF000);
+	PagingTable *table = (PagingTable*)(directory->entry[dirIndex] & _MASK);
 	table[tblIndex] = (uint32_t) physicalAddr | flags;
 
 	return SUCCESS;
