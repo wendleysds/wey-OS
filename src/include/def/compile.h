a90fc44a94d9e41f72f73bd577747a8441737759
Wendley Santos <wendley.sdsilva@gmail.com>
2025-07-26 00:47:47 -0300

    feat: compile macros

--- src/include/def/compile.h
+++ src/include/def/compile.h
@@ -4,5 +4,16 @@
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #define likely(x) __builtin_expect(!!(x), 1)
 #define __must_check __attribute__((__warn_unused_result__))
+#define __same_type(a, b) __builtin_types_compatible_p(typeof(a), typeof(b))
+#define __non_zero(e) (sizeof(char[1 - 2 * !!(e)]) - 1)
+
+#ifndef __builtin_choose_expr
+#define __builtin_choose_expr(cond, if_true, if_false) ((cond) ? (if_true) : (if_false))
+#endif
+
+#define __no_return __attribute__((noreturn))
+
+#define asmlinkage __attribute__((regparm(0)))
+#define stringfy(s) #s
 
 #endif
