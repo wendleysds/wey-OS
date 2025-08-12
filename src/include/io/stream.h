c47cea0ae0d2a21ef4258f3df48e7d4e9ba32203
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-12 00:40:21 -0300

    feat(stream): device
    each stream has a device attatched to work with

--- src/include/io/stream.h
+++ src/include/io/stream.h
@@ -1,6 +1,7 @@
 #ifndef _STREAM_H
 #define _STREAM_H
 
+#include <device.h>
 #include <stdint.h>
 
 #define SECTOR_SIZE 512
@@ -8,16 +9,17 @@
 #define SEEK_CUR 1
 
 struct Stream{
-	uint32_t cursor;
+	uint64_t cursor;
 	uint8_t cache[SECTOR_SIZE];
-	uint32_t cachedSectorLBA;
+	uint64_t cachedSectorLBA;
 	uint8_t cacheValid;
+	struct device* dev;
 };
 
-struct Stream* stream_new();
+struct Stream* stream_new(struct device* device);
 int stream_read(struct Stream* stream, void* buffer, int total);
 int stream_write(struct Stream *stream, const void *buffer, int total);
-int stream_seek(struct Stream* stream, uint32_t sector, uint8_t whence);
+int stream_seek(struct Stream* stream, uint64_t sector, uint8_t whence);
 int stream_dispose(struct Stream* ptr);
 
 #endif
