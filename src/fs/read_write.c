48961a86b5c55afba70ba2449f2306676bc04602
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-21 01:26:23 -0300

    feat(vfs): read/write

---
+++ src/fs/read_write.c
@@ -0,0 +1,34 @@
+#include <fs/vfs.h>
+#include <def/status.h>
+
+int vfs_read(struct file *file, void *buffer, uint32_t size){
+    if(!file || !file->f_op){
+        return READ_FAIL;
+    }
+
+    if(!file->f_op->read){
+        return NOT_SUPPORTED;
+    }
+    
+    return file->f_op->read(file, buffer, size);
+}
+
+int vfs_write(struct file *file, const void *buffer, uint32_t size){
+    if(!file || !file->f_op){
+        return WRITE_FAIL;
+    }
+
+    if(!file->f_op->write){
+        return NOT_SUPPORTED;
+    }
+    
+    return file->f_op->write(file, buffer, size);
+}
+
+int vfs_lseek(struct file *file, int offset, int whence){
+    if(!file || !file->f_op || !file->f_op->lseek){
+        return NOT_SUPPORTED;
+    }
+    
+    return file->f_op->lseek(file, offset, whence);
+}
