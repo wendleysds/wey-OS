925a506eea3e9cee68bf8502dd7c19e9163f67db
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-23 18:41:27 -0300

    feat: kzmalloc

--- src/include/memory/kheap.h
+++ src/include/memory/kheap.h
@@ -1,11 +1,21 @@
 #ifndef _KERNEL_HEAP_H
 #define _KERNEL_HEAP_H
 
+#include <lib/mem.h>
 #include <stddef.h>
 
 int init_kheap();
 void* kmalloc(size_t size);
 void* kcalloc(size_t nmemb, size_t size);
 void kfree(void* ptr);
+
+static inline void* kzalloc(size_t size) {
+	void* ptr = kmalloc(size);
+	if (ptr) {
+		memset(ptr, 0x0, size);
+	}
+
+	return ptr;
+}
 
 #endif
