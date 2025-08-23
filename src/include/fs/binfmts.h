70c499cc76ca4be7e14b400a524a4ccb5cbc4827
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-23 18:43:42 -0300

    refractor: binprm
    addapted with the new vfs + support to interp

--- src/include/fs/binfmts.h
+++ src/include/fs/binfmts.h
@@ -2,19 +2,32 @@
 #define _BINARY_FORMATS_H
 
 #include <core/process.h>
+#include <fs/vfs.h>
 #include <mmu.h>
 #include <stdint.h>
 
-struct binprm{
-    char* filename;
-    int argc, envc;
-    int fd;
+#define BINPRM_BUFF_SIZE 256
 
-    void* loadAddress;
+struct binprm{
+    int argc, envc;
+
     void* entryPoint;
+
+	uintptr_t curMemTop;
+
+	struct file* executable;
+	struct file* interpreter;
+    struct file* file;
+
+	const char* filename;
+	const char* interp;
+	const char* fdpath;
+
+    int execfd;
 
     struct mm_struct* mm;
 
+	char buff[BINPRM_BUFF_SIZE];
 }__attribute__((packed));
 
 struct binfmt{
