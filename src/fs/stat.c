ee88d2f584203d5937e2345e1555bf79a2a62536
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-23 23:49:35 -0300

    fix(vfs): wrong arguments to the getattr

--- src/fs/stat.c
+++ src/fs/stat.c
@@ -1,26 +1,37 @@
 #include <fs/vfs.h>
 #include <def/err.h>
+#include <def/config.h>
+#include <lib/string.h>
 
 int vfs_getattr(const char *restrict path, struct stat *restrict statbuf){
     if(!path || !statbuf){
         return INVALID_ARG;
     }
 
-    struct inode *ino = vfs_lookup(path);
-    if(IS_ERR(ino)){
-        return PTR_ERR(ino);
+	char pathcopy[PATH_MAX];
+	strcpy(pathcopy, path);
+
+	char* slash = strrchr(pathcopy, '/');
+	*slash = '\0';
+
+	char* filename = slash + 1;
+	char* dirs = pathcopy;
+
+    struct inode *dir = vfs_lookup(strlen(dirs) == 0 ? "/" : dirs);
+    if(IS_ERR(dir)){
+        return PTR_ERR(dir);
     }
 
     int res;
 
-    if(!ino->i_op || !ino->i_op->getattr){
+    if(!dir->i_op || !dir->i_op->getattr){
         res = NOT_SUPPORTED;
         goto out;
     }
 
-    res = ino->i_op->getattr(ino, path, statbuf);
+    res = dir->i_op->getattr(dir, filename, statbuf);
     
 out:
-    inode_dispose(ino);
+    inode_dispose(dir);
     return res;
 }
