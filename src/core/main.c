097fe23a2a64210492a9b4a1e18b5852b891ab39
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-29 14:02:43 -0700

    fix: jump to kmain
    resolved tripple fault when the terminal try to access the framebuffer memory addres

--- src/core/main.c
+++ src/core/main.c
@@ -1,6 +1,7 @@
 #include <memory/paging.h>
 #include <def/config.h>
 #include <def/compile.h>
+#include <drivers/terminal.h>
 
 #include <stddef.h>
 #include <stdint.h>
@@ -8,12 +9,29 @@
 #define __section(x) __attribute__((section(x)))
 #define _MASK 0xFFFFF000
 
+#define VIDEO_INFO_LOCATION 0x8000
+
+#define _hlt while(1){	__asm__ volatile ("hlt"); }
+
+extern void kmain();
+
 uintptr_t allocAddr __section(".bss.boot");
 size_t allocOff __section(".bss.boot");
 
 #define _MALLOC_INIT(addr) \
 	allocAddr = addr; \
 	allocOff = 0
+
+__section(".text.boot")
+static void* _memset(void* ptr, int c, unsigned long size){
+	char* temp = (char*) ptr;
+
+	for(unsigned long i = 0; i < size; i++){
+		temp[i] = (char) c;
+	}
+
+	return ptr;
+}
 
 __section(".text.boot")
 void* bumb_alloc(void* start, size_t* off, size_t size){
@@ -27,34 +45,9 @@ void* bumb_alloc(void* start, size_t* off, size_t size){
 
 __section(".text.boot")
 static inline void* malloc(size_t size){
-	return bumb_alloc((void*)allocAddr, &allocOff, size);
-}
-
-__section(".text.boot")
-static void _get_indexes(void* virtualAddr, uint32_t* outDirIndex, uint32_t* outTabIndex){
-	uintptr_t virt = (uintptr_t)virtualAddr;
-	*outDirIndex = virt >> 22;
-	*outTabIndex = (virt >> 12) & 0x3FF;
-}
-
-__section(".text.boot")
-static void _map(struct PagingDirectory* directory, void* virtualAddr, void* physicalAddr, uint8_t flags){
-
-	uint32_t dirIndex, tblIndex;
-	_get_indexes(virtualAddr, &dirIndex, &tblIndex);
-
-	PagingTable* table = (PagingTable*)(directory->entry[dirIndex] & _MASK);
-	table[tblIndex] = (uint32_t) physicalAddr | flags;
-}
-
-__section(".text.boot")
-static void _map_range(struct PagingDirectory* directory, int count, void* virtualAddr, void* physicalAddr, uint8_t flags){
-	for(int i = 0; i < count; i++){
-		_map(directory, virtualAddr, physicalAddr, flags);
-
-		virtualAddr = (void*)((uintptr_t)virtualAddr + PAGING_PAGE_SIZE);
-		physicalAddr = (void*)((uintptr_t)physicalAddr + PAGING_PAGE_SIZE);
-	}
+	void* addr = bumb_alloc((void*)allocAddr, &allocOff, size);
+	_memset(addr, 0x0, size);
+	return addr;
 }
 
 __section(".text.boot")
@@ -71,45 +64,71 @@ __section(".text.boot")
 static inline void _epg(void) {
 	__asm__ __volatile__ (
 			"mov %%cr0, %%eax\n\t"
-      "or  $0x80000000, %%eax\n\t"
-      "mov %%eax, %%cr0"
+			"or  $0x80000000, %%eax\n\t"
+			"mov %%eax, %%cr0\n\t"
+			"jmp flush\n\t"
+			"flush:"
       :
       :
       : "eax", "memory"
   );
 }
 
-__section(".text.boot") 
-static struct PagingDirectory* _pag_dir_new(uint32_t tablesAmount){
-	struct PagingDirectory* directory = malloc(sizeof(struct PagingDirectory));
-	uint32_t* entry = (uint32_t*)malloc(sizeof(uint32_t) * tablesAmount);
+__section(".text.boot")
+static inline void _jmp(void* addr) {
+	__asm__ __volatile__ (
+			"jmp %0\n\t"
+      :
+      : "r"(addr)
+  );
+}
 
-	uint32_t offset = 0;
-	uint8_t flags = (FPAGING_P | FPAGING_RW);
+__section(".text.boot")
+static void _get_indexes(void* virtualAddr, uint32_t* outDirIndex, uint32_t* outTabIndex){
+	uintptr_t virt = (uintptr_t)virtualAddr;
+	*outDirIndex = virt >> 22;
+	*outTabIndex = (virt >> 12) & 0x3FF;
+}
 
-	for (uint32_t i = 0; i < tablesAmount; i++){
-		PagingTable* table = (PagingTable*)malloc(sizeof(PagingTable) * PAGING_TOTAL_ENTRIES_PER_TABLE);
+__section(".text.boot")
+static void _map(struct PagingDirectory* directory, void* virtualAddr, void* physicalAddr, uint8_t flags){
+	uint32_t dirIndex, tblIndex;
+	_get_indexes(virtualAddr, &dirIndex, &tblIndex);
 
-		for (int j = 0; j < PAGING_TOTAL_ENTRIES_PER_TABLE; j++){
-			table[j] = (offset + (j * PAGING_PAGE_SIZE)) | flags;
-		}
+	PagingTable* table = 0x0;
+	if(!(directory->entry[dirIndex] & FPAGING_P)){
+		table = (PagingTable*)malloc(sizeof(PagingTable) * PAGING_TOTAL_ENTRIES_PER_TABLE);
 
-		offset += (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE);
-		entry[i] = (PagingTable)table | flags;
+		directory->entry[dirIndex] = (PagingTable)table | (FPAGING_P | FPAGING_RW);
+		directory->tableCount++;
+	}else{
+		table = (PagingTable*)(directory->entry[dirIndex] & _MASK);
 	}
 
-	// identity map entry
-	directory->entry = entry;
-	directory->tableCount = tablesAmount;
+	table[tblIndex] = (uint32_t) physicalAddr | flags;
+}
 
+__section(".text.boot")
+static void _map_range(struct PagingDirectory* directory, int count, void* virtualAddr, void* physicalAddr, uint8_t flags){
+	for(int i = 0; i < count; i++){
+		_map(directory, virtualAddr, physicalAddr, flags);
+
+		virtualAddr = (void*)((uintptr_t)virtualAddr + PAGING_PAGE_SIZE);
+		physicalAddr = (void*)((uintptr_t)physicalAddr + PAGING_PAGE_SIZE);
+	}
+}
+
+__section(".text.boot") 
+static struct PagingDirectory* _pag_dir_new_empty(){
+	struct PagingDirectory* directory = malloc(sizeof(struct PagingDirectory));
+	uint32_t* entry = (uint32_t*)malloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
+
+	directory->entry = entry;
 	return directory;
 }
 
 __section(".text.boot") 
-static void map_kernel_high(){
-
-	struct PagingDirectory* dir = _pag_dir_new(PAGING_TOTAL_ENTRIES_PER_TABLE);
-	
+static void _map_kernel(struct PagingDirectory* directory){
 	extern uintptr_t __kernel_phys_start;
 	extern uintptr_t __kernel_phys_end;
 	extern uintptr_t __kernel_high_start;
@@ -117,31 +136,86 @@ static void map_kernel_high(){
 	size_t kernel_size = (size_t)&__kernel_phys_end - (size_t)&__kernel_phys_start;
 	size_t kernel_pages = (kernel_size + PAGING_PAGE_SIZE - 1) / PAGING_PAGE_SIZE;
 
+	uint8_t flags = (FPAGING_P | FPAGING_RW);
+
+	// Identity
 	_map_range(
-			dir, 
-			kernel_pages,
+			directory,
+			PAGING_PAGE_SIZE / 4, // 0x00000000 -> 0x00400000
+			(void*)0x0,
+			(void*)0x0,
+			flags
+	);
+
+	// High
+	_map_range(
+			directory, 
+			PAGING_PAGE_SIZE + (kernel_pages - kernel_pages),
 			(void*)&__kernel_high_start,
       (void*)&__kernel_phys_start,
+			flags
+	);
+}
+
+__section(".text.boot")
+static void _map_heap(struct PagingDirectory* directory){
+	uint8_t flags = (FPAGING_P | FPAGING_RW);
+
+	size_t heap_pages = (HEAP_SIZE_BYTES + PAGING_PAGE_SIZE - 1) / PAGING_PAGE_SIZE;
+
+	_map_range(
+		directory, 
+		heap_pages,
+		(void*)HEAP_VIRT_BASE, 
+		(void*)HEAP_PHYS_BASE,  
+		flags
+	);
+
+	size_t table_size = (HEAP_SIZE_BYTES / HEAP_BLOCK_SIZE);
+	size_t table_pages = (table_size + PAGING_PAGE_SIZE - 1) / PAGING_PAGE_SIZE;
+
+	_map_range(
+		directory, 
+		table_pages,
+		(void*)HEAP_TABLE_VIRT_BASE, 
+		(void*)HEAP_TABLE_PHYS_BASE,  
+		flags
+	);
+}
+
+__section(".text.boot")
+static void _map_framebuffer(struct PagingDirectory* directory){
+	struct VideoStructPtr* video = (struct VideoStructPtr*)VIDEO_INFO_LOCATION;
+	size_t videoMemSize = video->height * video->width * (video->bpp / 8);
+
+	_map_range(
+			directory, 
+			(videoMemSize + PAGING_PAGE_SIZE - 1) / PAGING_PAGE_SIZE, 
+			(void*)KERNEL_FB_VIRT_BASE,
+			(void*)video->framebuffer_physical,
 			(FPAGING_P | FPAGING_RW)
 	);
 
-	_ldir(dir->entry);
+	video->framebuffer_virtual = KERNEL_FB_VIRT_BASE;
 }
 
 __section(".text.boot") 
 void main(){
 	_MALLOC_INIT(_TEMP_PAGE_DIRECTORY_ADDRESS);
 
-	map_kernel_high();
+	struct PagingDirectory* dir = _pag_dir_new_empty();
+
+	_map_kernel(dir);
+	_map_heap(dir);
+	_map_framebuffer(dir);
+	
+	_ldir(dir->entry);
+
 	_epg();
-
-	extern __no_return void kmain(void);
-
+	
 	void (*jmp_kmain)(void) = (void(*)(void))((uintptr_t)&kmain);
 	jmp_kmain();
 
-	while(1){
-		__asm__ volatile ("hlt");
-	}
+	_hlt
 }
 
