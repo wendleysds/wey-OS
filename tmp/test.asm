97da2661bcb0933fdf793315a45d1b2463fdf993
Wendley Santos <wendley.sdsilva@gmail.com>
2025-08-31 00:20:04 -0300

    feat: write_console test

--- tmp/test.asm
+++ tmp/test.asm
@@ -1,12 +1,13 @@
-[BITS 32]
+section .text
+    global _start
 
-global _start
+msg db 'Hello, World!', 0xA
+    len equ $ - msg
 
 _start:
-	mov eax, 1
-	mov ebx, 2
-	mov ecx, 3
-	mov edx, 4
-	int 0x80 ; syscall to isr80h_handler
-
+    mov eax, 1
+    mov ebx, msg
+    mov ecx, len
+    int 0x80
+	
 	jmp $
