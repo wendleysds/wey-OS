3c813eb1b968697beb5c093c5f4bb51a488c6f17
Wendley Santos <wendley.sdsilva@gmail.com>
2025-09-07 18:11:35 -0300

    refractor: removed syscall prototype

--- clib/src/stdio/put.c
+++ clib/src/stdio/put.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include <syscall.h>
 
-long syscall(long no, long arg1, long agr2, long arg3, long arg4);
-
 int putchar(int c){
 	char ch = (char)c;
     return syscall(SYS_write, (long)&ch, 1, 0, 0);
@@ -11,7 +9,7 @@ int putchar(int c){
 
 int puts(const char* str){
 	int len = strlen(str);
-	
+
 	syscall(SYS_write, (long)str, len, 0, 0);
 	putchar('\n');
 
