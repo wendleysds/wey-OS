fd90ee3cdfee9bb4786039b6afe4276798d713b9
Wendley Santos <wendley.sdsilva@gmail.com>
2025-09-07 18:10:35 -0300

    refactor: put*
    semanticaly iquals libc

--- clib/src/stdio/put.c
+++ clib/src/stdio/put.c
@@ -5,10 +5,15 @@
 long syscall(long no, long arg1, long agr2, long arg3, long arg4);
 
 int putchar(int c){
-	char buffer[] = { c, '\0'};
-	return syscall(SYS_write, (long)buffer, sizeof(buffer), 0, 0);
+	char ch = (char)c;
+    return syscall(SYS_write, (long)&ch, 1, 0, 0);
 }
 
 int puts(const char* str){
-	return syscall(SYS_write, (long)str, strlen(str), 0, 0);
+	int len = strlen(str);
+	
+	syscall(SYS_write, (long)str, len, 0, 0);
+	putchar('\n');
+
+	return len + 1;
 }
