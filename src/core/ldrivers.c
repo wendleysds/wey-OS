4da1a1b6ad38160f357da711802cbb59ffa87cbb
Wendley Santos <wendley.sdsilva@gmail.com>
2025-09-09 17:36:46 -0300

    refactor(dev): moved bdev to drivers/

--- src/core/ldrivers.c
+++ src/core/ldrivers.c
@@ -2,7 +2,15 @@
 #include <drivers/fat_fs.h>
 #include <drivers/keyboard.h>
 
+#include <def/config.h>
+
+extern struct device* devices[DEVICES_MAX];
+
 void load_drivers(){
+	for (int i = 0; i < DEVICES_MAX; i++){
+		devices[i] = 0x0;
+	}
+
     ata_init();
     fat_fs_init();
     keyboard_init();
