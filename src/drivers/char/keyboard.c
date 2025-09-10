8f77fbb9f6ebd824c14c1e79e50d135f98ad16f6
Wendley Santos <wendley.sdsilva@gmail.com>
2025-09-10 16:48:27 -0300

    feat: keyboard callback

--- src/drivers/char/keyboard.c
+++ src/drivers/char/keyboard.c
@@ -13,55 +13,29 @@
 #define _PS2_ENABLE_FIRST_PORT 0xAE
 
 #define _IQR_KEYBOARD_INTERRUPT 0x21
-
 #define _KEYBOARD_KEY_RELEASED 0x80
-#define _KEYBOARD_BUFFER_SIZE 256
 
-// qwert - US
-static const char _scancode_table[128] = {
+/*QWERT*/
+
+// US
+static const char _scancode_us_table[128] = {
     0,  27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b',
     '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n', 0,
     'a','s','d','f','g','h','j','k','l',';','\'','`',  0, '\\',
     'z','x','c','v','b','n','m',',','.','/',   0,   '*', 0, ' ',
 };
 
-static char _kb_buffer[_KEYBOARD_BUFFER_SIZE];
-static volatile int _kb_head = 0;
-static volatile int _kb_tail = 0;
+/*static const char _scancode_us_table_shift[128] = {
+	0,  27, '!','@','#','$','%','^','&','*','(',')','_','+', '\b',
+	'\t','Q','W','E','R','T','Y','U','I','O','P','{','}','\n', 0,
+	'A','S','D','F','G','H','J','K','L',':','"','~',  0, '|',
+	'Z','X','C','V','B','N','M','<','>','?',   0,   '*', 0, ' ',
+};*/
 
-static int listeners = 0;
+static keyboard_callback_t _kb_callback = 0x0;
 
-static inline void _kb_push(char c) {
-	if(listeners <= 0) {
-		return;
-	}
-
-	int next = (_kb_head + 1) % _KEYBOARD_BUFFER_SIZE;
-
-	if (next != _kb_tail) { // Check if buffer is full
-		_kb_buffer[_kb_head] = c;
-		_kb_head = next;
-	}
-}
-
-static inline char _kb_pop() {
-	if (_kb_head == _kb_tail) {
-		return 0; // Buffer is empty
-	}
-
-	char c = _kb_buffer[_kb_tail];
-	_kb_tail = (_kb_tail + 1) % _KEYBOARD_BUFFER_SIZE;
-	return c;
-}
-
-static inline void _kb_clear(char* kbbuffer, int size) {
-	for (int i = 0; i < size; i++) {
-		kbbuffer[i] = 0;
-	}
-
-	_kb_head = 0;
-	_kb_tail = 0;
-}
+/*static char shifted = 0;
+static char capslock = 0;*/
 
 static void _iqr_keyboard_handler(struct InterruptFrame* frame){
 	uint8_t scancode = inb(_PS2_INPUT_PORT);
@@ -70,10 +44,12 @@ static void _iqr_keyboard_handler(struct InterruptFrame* frame){
 		return;
 	}
 
-	if (scancode < sizeof(_scancode_table)) {
-		char key = _scancode_table[scancode];
+	if (scancode < sizeof(_scancode_us_table)) {
+		char key = _scancode_us_table[scancode];
 		if (key) {
-			_kb_push(key);
+			if(_kb_callback){
+				_kb_callback(key);
+			}	
 		}
 	}
 }
@@ -81,40 +57,9 @@ static void _iqr_keyboard_handler(struct InterruptFrame* frame){
 void keyboard_init(){
 	outb(_PS2_COMMAND_PORT, _PS2_ENABLE_FIRST_PORT);
 	idt_register_callback(_IQR_KEYBOARD_INTERRUPT, _iqr_keyboard_handler);
-
-	// Clear keyboard buffer
-	_kb_clear(_kb_buffer, _KEYBOARD_BUFFER_SIZE);
+	_kb_callback = 0x0;
 }
 
-char keyboard_getchar() {
-	return _kb_pop();
-}
-
-// Ring 0 keyboard read
-void keyboard_read(char* buffer, int size) {
-	if (!buffer || size <= 0) {
-		return; // Invalid arguments
-	}
-
-	listeners++;
-
-	int i = 0;
-	while (i < size - 1) {
-		char c = keyboard_getchar();
-		if (c == 0) {
-			__asm__ volatile ("hlt"); // Wait for input
-			continue;
-		}
-
-		terminal_write("%c", c);
-
-		if (c == '\n' || c == '\r') {
-			break; // End of input
-		}
-
-		buffer[i++] = c;
-	}
-
-	buffer[i] = '\0';
-	listeners--;
+void keyboard_set_callback(keyboard_callback_t callback){
+	_kb_callback = callback;
 }
--- src/include/drivers/keyboard.h
+++ src/include/drivers/keyboard.h
@@ -1,7 +1,9 @@
 #ifndef _KEYBOARD_H
 #define _KEYBOARD_H
 
+typedef void(*keyboard_callback_t)(char); 
+
 void keyboard_init();
-void keyboard_read(char* buffer, int size);
+void keyboard_set_callback(keyboard_callback_t callback);
 
 #endif
