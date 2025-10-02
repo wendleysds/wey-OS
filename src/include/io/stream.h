75cfb5eaf5d8c4a2054ffbab43993d64100d2745
Wendley Santos <wendley.sdsilva@gmail.com>
2025-10-02 14:21:18 -0300

    updated stream.h

--- src/include/io/stream.h
+++ src/include/io/stream.h
@@ -1,7 +1,8 @@
 #ifndef _STREAM_H
 #define _STREAM_H
 
-#include <device.h>
+#include <blkdev.h>
+#include <fs/vfs.h>
 #include <stdint.h>
 
 #define SECTOR_SIZE 512
@@ -9,19 +10,21 @@
 #define SEEK_CUR 1
 
 struct Stream{
-	uint64_t cursor;
 	uint8_t cache[SECTOR_SIZE];
 	uint64_t cachedSectorLBA;
 	uint8_t cacheValid;
-	struct device* dev;
+
+	struct file fbdev;
+	struct blkdev* bdev;
 };
 
-struct Stream* stream_new(struct device* device);
+struct Stream* stream_new(struct blkdev* bdev);
 int stream_read(struct Stream* stream, void* buffer, int total);
 int stream_write(struct Stream *stream, const void *buffer, int total);
-int stream_seek(struct Stream* stream, uint64_t sector, uint8_t whence);
-static inline uint64_t stream_tell(struct Stream* stream){
-	return stream->cursor;
+int stream_seek(struct Stream* stream, uint32_t offset, uint8_t whence);
+
+static inline uint32_t stream_tell(struct Stream* stream){
+	return stream->fbdev.pos;
 }
 
 int stream_dispose(struct Stream* ptr);
