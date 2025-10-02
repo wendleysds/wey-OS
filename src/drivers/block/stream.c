e1781f224b4e083477c7cb41ba611cc8f3f58de1
Wendley Santos <wendley.sdsilva@gmail.com>
2025-10-02 14:20:55 -0300

    refrator(stream): modified to the new api

--- src/drivers/block/stream.c
+++ src/drivers/block/stream.c
@@ -1,27 +1,32 @@
 #include <io/stream.h>
-#include <drivers/ata.h>
 #include <memory/kheap.h>
 #include <lib/mem.h>
-
 #include <def/status.h>
 
-struct Stream* stream_new(struct device* device){
-	if(!device) return 0x0;
-	struct Stream* s = (struct Stream*)kmalloc(sizeof(struct Stream));
-	if(!s) return 0x0;
-	
-	s->cursor = 0;
-	s->cacheValid = 0;
-	s->dev = device;
-	memset(s->cache, 0x0, sizeof(s->cache));
+struct Stream* stream_new(struct blkdev* bdev){
+	if(!bdev) return 0x0;
 
+	struct Stream* s = (struct Stream*)kzalloc(sizeof(struct Stream));
+	if(s){
+		s->cacheValid = 0;
+		s->bdev = bdev;
+
+		s->fbdev.f_op = (struct file_operations*)bdev->ops;
+		s->fbdev.private_data = bdev->dev->driver_data;
+	}
+	
 	return s;
 }
 
 struct Stream* stream_dup(struct Stream* stream){
+	if(!stream) return 0x0;
+
 	struct Stream* s = (struct Stream*)kmalloc(sizeof(struct Stream));
-	if(!s) return 0x0;
-	return memcpy(s, stream, sizeof(struct Stream));
+	if(s){
+		memcpy(s, stream, sizeof(struct Stream));
+	}
+
+	return s;
 }
 
 int stream_read(struct Stream *stream, void *buffer, int total){
@@ -33,16 +38,16 @@ int stream_read(struct Stream *stream, void *buffer, int total){
 	uint8_t* bufPtr = (uint8_t*)buffer;
 
 	while(totalRemaining > 0){
-		uint32_t sector = stream->cursor / SECTOR_SIZE;
-		uint64_t offset = stream->cursor % SECTOR_SIZE;
+		uint32_t sector = stream->fbdev.pos / SECTOR_SIZE;
+		uint64_t offset = stream->fbdev.pos % SECTOR_SIZE;
 
 		// Check if we need to read a new sector into the cache
 		// If the cache is invalid or the sector is different, read it
 		// from the disk
 		if (!stream->cacheValid || stream->cachedSectorLBA != sector) {
-			int status = stream->dev->read(
-				stream->dev, stream->cache,
-				sizeof(stream->cache), (stream->cursor &= ~(SECTOR_SIZE - 1))
+			int status = stream->bdev->ops->read(
+				&stream->fbdev, stream->cache,
+				sizeof(stream->cache)
 			);
 
             if (status < 0) return status;
@@ -58,7 +63,7 @@ int stream_read(struct Stream *stream, void *buffer, int total){
 
 		// Update pointers and counters
 		bufPtr += toRead;
-		stream->cursor += toRead;
+		stream->fbdev.pos += toRead;
 		totalRemaining -= toRead;
 	}
 
@@ -74,13 +79,12 @@ int stream_write(struct Stream *stream, const void *buffer, int total){
 	uint8_t* bufPtr = (uint8_t*)buffer;
 
 	while(totalRemaining > 0){
-		uint32_t sector = (stream->cursor & ~(SECTOR_SIZE - 1));
-		uint64_t offset = stream->cursor % SECTOR_SIZE;
+		uint64_t offset = stream->fbdev.pos % SECTOR_SIZE;
 
 		char cache[SECTOR_SIZE];
-		int readStatus = stream->dev->read(
-			stream->dev, cache, 
-			sizeof(cache), sector
+		int readStatus = stream->bdev->ops->read(
+				&stream->fbdev, stream->cache,
+				sizeof(stream->cache)
 		);
 
 		if (readStatus < 0) return readStatus;
@@ -90,42 +94,34 @@ int stream_write(struct Stream *stream, const void *buffer, int total){
 
 		memcpy(cache + offset, bufPtr, toWrite);
 
-		int writeStatus = stream->dev->write(
-			stream->dev, cache, 
-			sizeof(cache), sector
+		int writeStatus = stream->bdev->ops->write(
+			&stream->fbdev, stream->cache,
+			sizeof(stream->cache)
 		);
 
 		if(writeStatus < 0) return writeStatus;
 
 		// Update pointers and counters
 		bufPtr += toWrite;
-		stream->cursor += toWrite;
+		stream->fbdev.pos += toWrite;
 		totalRemaining -= toWrite;
 	}
 
 	return SUCCESS;
 }
 
-int stream_seek(struct Stream *stream, uint64_t sector, uint8_t whence){
-	if(!stream || sector < 0){
+int stream_seek(struct Stream *stream, uint32_t offset, uint8_t whence){
+	if(!stream || offset < 0){
 		return INVALID_ARG;
 	}
 
-	switch (whence)
-	{
-		case SEEK_SET:
-			stream->cursor = sector;
-			stream->cacheValid = 0;
-			break;
-		case SEEK_CUR:
-			stream->cursor += sector;
-			break;
-		default:
-			return INVALID_ARG; // Invalid whence
-			break;
+	if(!stream->bdev->ops->lseek){
+		return NOT_SUPPORTED;
 	}
 
-	return SUCCESS;
+	int res = stream->bdev->ops->lseek(&stream->fbdev, offset, whence);
+
+	return res;
 }
 
 int stream_dispose(struct Stream *ptr){
