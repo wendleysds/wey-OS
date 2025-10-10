e14ad586ca6210bcc0cd62464f360a14c7a3002d
Wendley Santos <wendley.sdsilva@gmail.com>
2025-10-09 23:47:31 -0300

    feat: finished intcall

--- src/boot/bios.asm
+++ src/boot/bios.asm
@@ -16,10 +16,9 @@ bios_putchar:
 
 ; void intcall(uint8_t int_no, const struct biosregs *ireg, struct biosregs *oreg);
 intcall:
-	mov bp, sp
-	mov al, [bp+4]  ; int_no
-	mov dx, [bp+8]  ; ireg
-	mov cx, [bp+12] ; oreg
+	mov al, [esp +4]  ; int_no
+	mov dx, [esp +8]  ; ireg
+	mov cx, [esp +12] ; oreg
 
 	cmp al, byte [no]
 	je .do
@@ -28,8 +27,8 @@ intcall:
 
 .do:
 	pushfd
-	push fs
-	push gs
+	push word fs
+	push word gs
 	pushad
 
 	; Copy ireg to stack frame
@@ -39,15 +38,50 @@ intcall:
 	mov cx, 11
 	rep movsd
 
-	; set registers poping from stack frame
+	; Set registers poping from stack frame
 	popad
-	pop gs
-	pop fs
-	pop es
-	pop ds
+	pop word gs
+	pop word fs
+	pop word es
+	pop word ds
 	popfd
 
 	db 0xCD
 no: db 0x0
+
+	; Save cpu current state on stack
+	pushfd
+	push word ds
+	push word es
+	push word fs
+	push word gs
+	pushad
+
+	; Re-establish C environment invariants
+	cld
+	movzx esp, sp
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
+
+	; Get 3rd argument
+	mov di, [esp+68]
+	and di, di ; test if oreg is null
+	jz .out
+
+	; Copy cpu state from stack to frame
+	mov si, sp
+	mov cx, 11
+	rep movsd
+
+.out:
+	; clear buffer
+	add sp, 44
+
+	; restore
+	popad
+	pop word gs
+	pop word fs
+	popfd
 
 	ret
