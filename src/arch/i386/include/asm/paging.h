3003ad8937b062ab583ce65d41f6927ccda2c997
Wendley Santos <wendley.sdsilva@gmail.com>
2025-10-30 16:54:45 -0300

    refractor(paging): moved to arch/
    rewrited the api to a more eficiente one

---
+++ src/arch/i386/include/asm/paging.h
@@ -0,0 +1,46 @@
+#ifndef _PAGING_i386_H
+#define _PAGING_i386_H
+
+#include <stdint.h>
+
+#define PGD_MAX_PTE 1024
+#define PTE_MAX_ENTRIES 1024
+#define PTE_PAGE_SIZE 4096
+
+// Flags
+#define _PAGE_PCD 0x16
+#define _PAGE_PWT 0x8
+#define _PAGE_US  0x4
+#define _PAGE_RW  0x2
+#define _PAGE_P   0x1
+
+#define PTE_FLAGS 0xFFFFF000
+
+#define PAGE_MASK 0xFFFFF000
+#define FLAGS_MASK 0x00000FFF
+
+typedef unsigned long pte_t;
+typedef unsigned long pgd_t;
+
+extern pgd_t* _current_pgd;
+
+static inline void invlpg(uintptr_t virtaddr){
+	__asm__ volatile("invlpg (%0)" : : "r"(virtaddr) : "memory");
+}
+
+int pgd_load(pgd_t* pgd);
+
+pgd_t* pgd_alloc();
+void pgd_free(pgd_t* pgd);
+
+int pgd_map(uintptr_t virtaddr, uintptr_t physaddr, mem_flags_t flags);
+int pgd_unmap(uintptr_t virtaddr);
+
+int pte_update_flags(uintptr_t virtaddr, mem_flags_t flags);
+void* pgd_translate(void* virtaddr);
+
+int pgd_dup_current(pgd_t* dest, uint8_t copy_kernel_area);
+int pgd_copy(pgd_t* dest, pgd_t* src);
+
+#endif
+
---
+++ src/arch/i386/mm/paging.c
@@ -0,0 +1,227 @@
+#include <asm/paging.h>
+#include <wey/mmu.h>
+#include <mm/kheap.h>
+#include <def/err.h>
+#include <def/config.h>
+
+#define ADDRS_NOT_ALING(virt, phys) \
+	(((uintptr_t)(virt) & (PTE_PAGE_SIZE - 1)) || \
+	((uintptr_t)(phys) & (PTE_PAGE_SIZE - 1)))
+
+#define SELF_PDE_INDEX 1023
+#define SELF_PDE_BASE  (SELF_PDE_INDEX << 22)
+
+// pgd virtual table base address
+#define PGD_VADDR ((pgd_t*)0xFFFFF000)
+
+// pte virtual table base address
+#define PT_VADDR(d) ((void*)(SELF_PDE_BASE + ((d) << 12)))
+
+// pte virtual table address
+#define PTE_VADDR(d,t) ((void*)(SELF_PDE_BASE + ((d) << 12) + ((t) << 2)))
+
+// *PDE_VADDR(d) == PGD_VADDR[d]
+#define PDE_VADDR(d) ((unsigned long*)(0xFFFFF000 + ((d) << 2))) 
+
+pgd_t* _current_pgd = NULL;
+
+int pgd_load(pgd_t* pgd){
+	if(!pgd){
+		return NULL_PTR;
+	}
+
+	if(_current_pgd == pgd){
+		return SUCCESS;
+	}
+
+	if ((uintptr_t)(pgd) & (PTE_PAGE_SIZE - 1)) {
+		return BAD_ALIGNMENT;
+	}
+
+	__asm__ volatile (
+		"mov %0, %%cr3"
+		:
+		: "r"(pgd_translate(pgd))
+		: "memory"
+	);
+
+	_current_pgd = pgd;
+	
+	return SUCCESS;
+}
+
+pgd_t* pgd_alloc(){
+	return kcalloc(PGD_MAX_PTE, sizeof(pgd_t));
+}
+
+void pgd_free(pgd_t* pgd){
+	if(pgd == _current_pgd){
+		panic("paging_free_directory(): Trying to free current directory!");
+	}
+
+	for (int i = 0; i < PGD_MAX_PTE; i++) {
+		if (pgd[i] & _PAGE_P) {
+			kfree_phys(pgd[i] & PAGE_MASK);
+		}
+	}
+
+	kfree(pgd);	
+}
+
+int pgd_map(uintptr_t virtaddr, uintptr_t physaddr, mem_flags_t flags){
+	if(ADDRS_NOT_ALING(virtaddr, physaddr)){
+		return BAD_ALIGNMENT;
+	}
+
+	uint32_t dir_idx = virtaddr >> 22;
+	uint32_t tbl_idx = (virtaddr >> 12) & 0x3FF;
+
+	pgd_t* pde = PGD_VADDR;
+	pte_t* pte = PT_VADDR(dir_idx);
+
+	uint8_t pg_flags = _PAGE_P;
+	if(flags & MEM_WRITE)   pg_flags |= _PAGE_RW;
+	if(flags & MEM_USER)    pg_flags |= _PAGE_US;
+
+	if (!(pde[dir_idx] & _PAGE_P)) {
+		void* newTable = kcalloc(PTE_MAX_ENTRIES, sizeof(pte_t));
+		if (!newTable) {
+			return NO_MEMORY;
+		}
+
+		pde[dir_idx] = (pte_t)pgd_translate(newTable) | pg_flags;
+		pte = VIRT_PTBL(dir_idx);
+	}
+
+	if (pte[tbl_idx] & _PAGE_P) {
+		return ALREADY_MAPD;
+	}
+
+	pte[tbl_idx] = ((uintptr_t)physaddr & PAGE_MASK) | pg_flags;
+
+	return SUCCESS;
+}
+
+int pgd_unmap(uintptr_t virtaddr){
+	uint32_t dir_idx = virtaddr >> 22;
+	uint32_t tbl_idx = (virtaddr >> 12) & 0x3FF;
+
+	pgd_t* pde = PGD_VADDR;
+	if (!(pde[dir_idx] & _PAGE_P)) {
+		return ALREADY_UMAPD;
+	}
+
+	pte_t* pte = VIRT_PTBL(dir_idx);
+	if (!(pte[tbl_idx] & _PAGE_P)) {
+		return ALREADY_UMAPD;
+	}
+
+	pte[tbl_idx] = 0;
+	invlpg(virtaddr);
+
+	for (int i = 0; i < PTE_MAX_ENTRIES; i++) {
+		if (pte[i] & _PAGE_P) {
+			return SUCCESS;
+		}
+	}
+
+	kfree_phys(pde[dir_idx] & PAGE_MASK);
+	pde[dir_idx] = 0;
+
+	invlpg((void*)(dir_idx << 22));
+
+	return SUCCESS;
+}
+
+int pte_update_flags(uintptr_t virtaddr, mem_flags_t flags){
+	uint32_t dir_idx = virtaddr >> 22;
+	uint32_t tbl_idx = (virtaddr >> 12) & 0x3FF;
+
+	pgd_t* pde = PGD_VADDR;
+	if (!(pde[dir_idx] & _PAGE_P)) {
+		return INVALID_ARG;
+	}
+
+	pte_t* pte = PT_VADDR(dir_idx);
+	if (!(pte[tbl_idx] & _PAGE_P)) {
+		return INVALID_ARG;
+	}
+
+	uintptr_t physaddr = pte[tbl_idx] & PAGE_MASK;
+
+	uint8_t pg_flags = _PAGE_P;
+	if(flags & MEM_WRITE)   pg_flags |= _PAGE_RW;
+	if(flags & MEM_USER)    pg_flags |= _PAGE_US;
+
+	pte[tbl_idx] = physaddr | pg_flags;
+	invlpg(virtaddr);
+
+	return SUCCESS;
+}
+
+void* pgd_translate(void* virtaddr){
+	uint32_t dir_idx = (uintptr_t)virtaddr >> 22;
+	uint32_t tbl_idx = ((uintptr_t)virtaddr >> 12) & 0x3FF;
+	uint32_t offset  = (uintptr_t)virtaddr & 0xFFF;
+
+	pgd_t* pde = PGD_VADDR;
+	if (!(pde[dir_idx] & _PAGE_P)) {
+		return NULL;
+	}
+
+	pte_t* pte = PT_VADDR(dir_idx);
+	if (!(pte[tbl_idx] & _PAGE_P)) {
+		return NULL;
+	}
+
+	uintptr_t physaddr = (pte[tbl_idx] & PAGE_MASK) | offset;
+	return (void*)physaddr;
+}
+
+// duplicate page directory from current loaded directory(because self mapping is used) to dest
+int pgd_dup_current(pgd_t* dest, uint8_t copy_kernel_area){
+
+	pgd_t* src = PGD_VADDR;
+
+	for (int i = 0; i < PGD_MAX_PTE; i++){
+		if (!(src[i] & _PAGE_P)) {
+			dest[i] = 0;
+			continue;
+		}
+
+		if (i >= (KERNEL_VIRT_BASE >> 22) && !copy_kernel_area) {
+			dest[i] = src[i];
+			continue;
+		}
+
+		if (src[i] & _PAGE_P) {			
+			pte_t* src_pt = PT_VADDR(i);
+			pte_t* dest_pt = kcalloc(PTE_MAX_ENTRIES, sizeof(pte_t));
+			if (!dest_pt) {
+				for(int j = 0; j < i; j++){
+					if(dest[j] & _PAGE_P){
+						kfree_phys(dest[j] & PAGE_MASK);
+					}
+				}
+
+				return NO_MEMORY;
+			}
+
+			for (int j = 0; j < PTE_MAX_ENTRIES; j++) {
+				dest_pt[j] = src_pt[j];
+			}
+
+			dest[i] = (pgd_t)pgd_translate(dest_pt) | (src[i] & FLAGS_MASK);
+		}
+	}
+
+	return SUCCESS;
+}
+
+int pgd_copy(pgd_t* dest, pgd_t* src){
+	for (int i = 0; i < PGD_MAX_PTE; i++) {
+		dest[i] = src[i];
+	}
+
+	return SUCCESS;
+}
