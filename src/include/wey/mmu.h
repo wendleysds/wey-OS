370fed0d33ea4f874f221813ff4654d4f29e4622
Wendley Santos <wendley.sdsilva@gmail.com>
2025-11-01 01:51:21 -0300

    refractor(mmu): adapted for the new API

--- src/include/wey/mmu.h
+++ src/include/wey/mmu.h
@@ -1,66 +1,19 @@
-#ifndef _MEMORY_MAP_H
-#define _MEMORY_MAP_H
+#ifndef _MEMORY_MANAGER_UNIT_H
+#define _MEMORY_MANAGER_UNIT_H
 
-#include <mm/paging.h>
-#include <mm/kheap.h>
-#include <lib/string.h>
-#include <stdint.h>
-
-#define PROT_NONE  0x0
-#define PROT_READ  0x1
-#define PROT_WRITE 0x2
-#define PROT_EXEC  0x4
-
-#define MAP_POPULATE  0x0
-#define MAP_FIXED     0x1
-#define MAP_PRIVATE   0x2
-#define MAP_ANONYMOUS 0x4
-
-#define SELF_PDE_INDEX 1023
-
-#define VIRT_PDIR ((uint32_t*)0xFFFFF000)
-#define VIRT_PTBL(i) ((uint32_t*)(0xFFC00000 + (i) * PAGING_PAGE_SIZE))
-
-struct mem_region {
-	void* physBaseAddress;
-    void* virtualBaseAddress;
-    void* virtualEndAddress;
-	
-	uint32_t size;
-	uint8_t flags;
-
-	uint8_t isPrivite;
-    struct mem_region* next;
-}__attribute__((packed));
-
-struct mm_struct{
-    struct mem_region* regions;
-    void* mmapBase; // Next free space
-
-    struct PagingDirectory* pageDirectory;
-};
-
-extern struct PagingDirectory* _currentDirectory;
+struct mm_struct;
 
 int mmu_init();
 
-struct PagingDirectory* mmu_create_page();
-int mmu_page_switch(struct PagingDirectory* directory);
-int mmu_destroy_page(struct PagingDirectory* directory);
-int mmu_map_pages(void* virtualAddr, void* physicalAddr, uint32_t size, uint8_t flags);
-int mmu_unmap_pages(void* virtualStart, uint32_t size);
+pgd_t* mmu_create_page();
+
+int mmu_mmap(struct mm_struct* mm, void* paddr, void* vaddr, int size, mem_flags_t flags);
+int mmu_munmap(struct mm_struct* mm, void* vaddr, int size);
+int mmu_set_flags(struct mm_struct* mm, void* paddr, int size, mem_flags_t flags);
+
+int mmu_page_switch(pgd_t* pgd);
+int mmu_destroy_page(pgd_t* pgd);
+
 void* mmu_translate(void* virt);
-uint8_t mmu_user_pointer_valid(void* ptr);
-uint8_t mmu_user_pointer_valid_range(const void* userPtr, size_t size);
-
-void mmu_copy_kernel_to_directory(struct PagingDirectory* directory);
-
-struct mem_region* vma_lookup(struct mm_struct* mm, void* virtualAddr);
-int vma_add(struct mm_struct* mm, void* virtualAddr, void* physicalAddr, uint32_t size, uint8_t flags, uint8_t isPrivate);
-int vma_remove(struct mm_struct* mm, void* virtualAddr, uint32_t size);
-int vma_clean(struct mm_struct* mm);
-int vma_destroy(struct mm_struct* mm);
-
-void* phys_to_virt(void* physicalAddr);
 
 #endif
--- src/memory/mmu.c
+++ src/memory/mmu.c
@@ -1,343 +1,208 @@
+#include <wey/mm_types.h>
+#include <asm/paging.h>
 #include <wey/mmu.h>
+#include <wey/vfs.h>
+#include <wey/vma.h>
 #include <wey/panic.h>
-#include <wey/sched.h>
-#include <mm/paging.h>
-#include <def/config.h>
 #include <def/err.h>
-#include <arch/i386/idt.h>
-#include <drivers/terminal.h>
-#include <stddef.h>
-#include <stdint.h>
 
-#define _ADDRS_NOT_ALING(virt, phys) \
-	(((uintptr_t)(virt) & (PAGING_PAGE_SIZE - 1)) || \
-	((uintptr_t)(phys) & (PAGING_PAGE_SIZE - 1)))
-
-static struct PagingDirectory* _kernelDirectory = 0x0;
-struct PagingDirectory* _currentDirectory = 0x0;
-
-static inline int _read_cr2(){
-	uint32_t cr2;
-	__asm__ volatile("mov %%cr2, %0" : "=r" (cr2));
-	return cr2;
-}
-
-typedef struct {
-	void* addr;
-	uint32_t ec;
-	int8_t present, write, user, rsvd, exec;
-} pf_info_t;
-
-static inline pf_info_t pf_decode(uint32_t ec, void* cr2){
-	pf_info_t pf;
-	memset(&pf, 0x0, sizeof(pf));
-
-	pf.addr = cr2;
-	pf.ec = ec;
-	pf.present = ec >> 0 & 1;
-	pf.write   = ec >> 1 & 1;
-	pf.user    = ec >> 2 & 1;
-	pf.rsvd    = ec >> 3 & 1;
-	pf.exec    = ec >> 4 & 1;
-
-	return pf;
-}
-
-static inline void _show_pf_info(pf_info_t pf){
-	terminal_write(
-		"\nPage fault at 0x%x (ec=0x%x) [present=%d, write=%d, user=%d, rsvd=%d, exec=%d]\n", 
-		pf.addr, pf.ec, pf.present, pf.write, pf.user, pf.rsvd, pf.exec
-	);
-}
-
-static void _page_fault_handler(struct InterruptFrame* frame){
-	void* faultingAddress = (void*)frame->eip;
-	pf_info_t pf = pf_decode(frame->err_code, faultingAddress);
-
-	struct mm_struct* mm = 0x0;
-	struct Task* task = pcb_current();
-
-	_show_pf_info(pf);
-
-	if(task && task->process){
-		mm = task->process->mm;
-	}else{
-		panic("Page fault in kernel mode!");
-	}
-
-	struct mem_region* v = vma_lookup(mm, faultingAddress);
-
-	if(!v){
-		panic(
-			"Invalid memory access in task %d (%s)\n",
-			task->tid, task->process ? task->process->name : "unknown"
-		);
-	}
-
-	if(!pf.present){
-		panic(
-			"Protection fault in task %d (%s)\n", 
-			task->tid, task->process ? task->process->name : "unknown"
-		);
-	}
-
-	// kill process
-	
-	while(1){
-		__asm__ volatile ("hlt");
-	}
-}
-
-static int _map_kernel(struct PagingDirectory* directory, void* virtualAddr, void* physicalAddr, uint8_t flags){
-	if(_ADDRS_NOT_ALING(virtualAddr, physicalAddr)){
-		return BAD_ALIGNMENT;
-	}
-
-	uint32_t dirIndex, tblIndex;
-
-	uintptr_t virt = (uintptr_t)virtualAddr;
-	dirIndex = virt >> 22;
-	tblIndex = (virt >> 12) & 0x3FF;
-
-	PagingTable* table = 0x0;
-	if(!(directory->entry[dirIndex] & FPAGING_P)){
-		table = (PagingTable*)kcalloc(sizeof(PagingTable), PAGING_TOTAL_ENTRIES_PER_TABLE);
-		if (!table){
-			return NO_MEMORY;
-		}
-
-		directory->tableCount++;
-		directory->entry[dirIndex] = (PagingTable)mmu_translate(table) | flags;
-		directory->virt_addrs[dirIndex] = (PagingTable)table | flags;
-	}else{
-		uintptr_t tableVirt = (uintptr_t)phys_to_virt((void*)directory->entry[dirIndex]);
-		table = (PagingTable*)(tableVirt & PAGE_MASK);
-	}
-
-	if (table[tblIndex] & FPAGING_P) {
-		return ALREADY_MAPD;
-	}
-
-	table[tblIndex] = (uint32_t) physicalAddr | flags;
-
-	return SUCCESS;
-}
-
-static int _map_kernel_range(struct PagingDirectory* directory, void* virtualAddr, void* physicalAddr, uint32_t size, uint8_t flags){
-	uint32_t alignedSize = (size + PAGING_PAGE_SIZE - 1) & ~(PAGING_PAGE_SIZE - 1);
-	uint32_t count = alignedSize / PAGING_PAGE_SIZE;
-
-	virtualAddr = paging_align_to_lower(virtualAddr);
-	physicalAddr = paging_align_to_lower(physicalAddr);
-
-	for(int i = 0; i < count; i++){
-		int status = _map_kernel(directory, virtualAddr, physicalAddr, flags);
-		if(status != SUCCESS)
-			return status;
-
-		virtualAddr = (void*)((uintptr_t)virtualAddr + PAGING_PAGE_SIZE);
-		physicalAddr = (void*)((uintptr_t)physicalAddr + PAGING_PAGE_SIZE);
-	}
-
-	return SUCCESS;
-}
+pgd_t* _kernel_pgd = NULL;
 
 int mmu_init(){
-	_currentDirectory = 0x0;
-	_kernelDirectory = 0x0;
+	_kernel_pgd = pgd_alloc();
+	if(!_kernel_pgd){
+		return NO_MEMORY;
+	}
 
-	struct PagingDirectory* dir = mmu_create_page();
+	int res = pgd_dup_current(_kernel_pgd, 1);
+	if(IS_STAT_ERR(res)){
+		panic("mmu_init(): pgd dup failed! %d", res);
+	}
 
-	extern uintptr_t __kernel_phys_start;
-	extern uintptr_t __kernel_phys_end;
-	extern uintptr_t __kernel_high_start;
+	res = pgd_load(_kernel_pgd);
+	if(IS_STAT_ERR(res)){
+		panic("mmu_init(): pgd kernel load failed! %d", res);
+	}
 
-	size_t kernel_size = (size_t)&__kernel_phys_end - (size_t)&__kernel_phys_start;
+	extern uintptr_t __boot_end;
 
-	uint8_t flags = (FPAGING_P | FPAGING_RW);
-
-	int res = _map_kernel_range(
-		dir,
-		(void*)&__kernel_high_start, 
-		(void*)&__kernel_phys_start, 
-		kernel_size,
-		flags
+	res = mmu_munmap(
+		NULL, 
+		(void*)0x8000, 
+		((size_t)&__boot_end - 0x8000)
 	);
 
 	if(IS_STAT_ERR(res)){
-		return res;
+		panic("mmu_init(): Failed to unmap boot area! %d", res);
 	}
 
-	res = _map_kernel_range(
-		dir,
-		(void*)KERNEL_STACK_VIRT_BOTTOM,
-		(void*)KERNEL_STACK_PHYS_BOTTOM,
-		KERNEL_STACK_SIZE,
-		flags
+	extern uintptr_t __kernel_rodata_start;
+	extern uintptr_t __kernel_rodata_end;
+
+	res = mmu_set_flags(
+		NULL,
+		&__kernel_rodata_start,
+		(size_t)&__kernel_rodata_end - (size_t)&__kernel_rodata_start,
+		(MEM_READ | MEM_KERNEL)
 	);
 
-	if(IS_STAT_ERR(res)){
-		return res;
-	}
-
-	res = _map_kernel_range(
-		dir,
-		(void*)HEAP_VIRT_BASE,
-		(void*)HEAP_PHYS_BASE,
-		HEAP_SIZE_BYTES,
-		flags
-	);
-
-	if(IS_STAT_ERR(res)){
-		return res;
-	}
-
-	res = _map_kernel_range(
-		dir, 
-		(void*)HEAP_TABLE_VIRT_BASE, 
-		(void*)HEAP_TABLE_PHYS_BASE, 
-		(HEAP_SIZE_BYTES / HEAP_BLOCK_SIZE), 
-		flags
-	);
-
-	if(IS_STAT_ERR(res)){
-		return res;
-	}
-
-	extern struct VideoStructPtr* _get_video();
-	struct VideoStructPtr* vPtr = _get_video();
-
-	res = _map_kernel_range(
-		dir,
-		(void*)KERNEL_FB_VIRT_BASE,
-		(void*)vPtr->framebuffer_physical,
-		(vPtr->height * vPtr->width * (vPtr->bpp / 8)),
-		flags
-	);
-
-	if(IS_STAT_ERR(res)){
-		return res;
-	}
-	
-	idt_register_callback(14, &_page_fault_handler);
-
-	_kernelDirectory = dir;
-	return mmu_page_switch(dir);
+	return res;
 }
 
-struct PagingDirectory* mmu_create_page(){
-	struct PagingDirectory* dir = paging_new_directory();
-	if(!dir){
-		return ERR_PTR(NO_MEMORY);
-	}
-
-	dir->entry[SELF_PDE_INDEX] = (PagingTable)((uintptr_t)mmu_translate(dir->entry) | FPAGING_P | FPAGING_RW);
-	dir->tableCount = 1;
-
-	return dir;
-}
-
-int mmu_page_switch(struct PagingDirectory* directory){
-	if(!directory){
-		return NULL_PTR;
-	}
-
-	if (_currentDirectory == directory) {
-		return SUCCESS;
-	}
-
-	if (!directory->entry) {
-		return NULL_PTR;
-	}
-
-	if ((uintptr_t)(directory->entry) & (PAGING_PAGE_SIZE - 1)) {
-		return BAD_ALIGNMENT;
-	}
-
-	if (directory->tableCount == 0) {
+int mmu_mmap(struct mm_struct* mm, void* physaddr, void* virtaddr, int size, mem_flags_t flags){
+	if(!physaddr || !virtaddr || size <= 0){
 		return INVALID_ARG;
 	}
 
-	if (directory->tableCount > PAGING_TOTAL_ENTRIES_PER_TABLE) {
-		return OUT_OF_BOUNDS;
-	}
+	uintptr_t paddr = (uintptr_t)physaddr;
+	uintptr_t vaddr = (uintptr_t)virtaddr;
 
-	uint32_t* physAddr = (uint32_t*)mmu_translate(directory->entry);
+	unsigned int alignedSize = (size + PTE_PAGE_SIZE - 1) & ~(PTE_PAGE_SIZE - 1);
+	unsigned int count = alignedSize / PTE_PAGE_SIZE;
 
-	paging_load_directory(physAddr);
-	_currentDirectory = directory;
-
-	return SUCCESS;
-}
-
-int mmu_destroy_page(struct PagingDirectory* directory){
-	if(!directory){
-		return NULL_PTR;
-	}
-
-	if(directory == _kernelDirectory){
-		return INVALID_ARG;
-	}else{
-		mmu_page_switch(_kernelDirectory);
-	}
-
-	paging_free_directory(directory);
-
-	return SUCCESS;
-}
-
-int mmu_map_pages(void* virtualAddr, void* physicalAddr, uint32_t size, uint8_t flags){
-	uint32_t alignedSize = (size + PAGING_PAGE_SIZE - 1) & ~(PAGING_PAGE_SIZE - 1);
-	uint32_t count = alignedSize / PAGING_PAGE_SIZE;
-
-	return paging_map_range(count, paging_align_to_lower(virtualAddr),  paging_align_to_lower(physicalAddr), flags);
-}
-
-int mmu_unmap_pages(void* virtualStart, uint32_t size){
-	uint32_t alignedSize = (size + PAGING_PAGE_SIZE - 1) & ~(PAGING_PAGE_SIZE - 1);
-	uint32_t count = alignedSize / PAGING_PAGE_SIZE;
-
-	return paging_unmap_range(count, paging_align_to_lower(virtualStart));
-}
-
-void* mmu_translate(void* virtualAddr){
-	return paging_translate(virtualAddr);
-}
-
-uint8_t mmu_user_pointer_valid(void* ptr){
-	if(!ptr){
-		return 0;
-	}
-	
-	return 0;
-}
-
-uint8_t mmu_user_pointer_valid_range(const void* userPtr, size_t size){
-    uintptr_t start = (uintptr_t)userPtr;
-    uintptr_t end = start + size;
-
-    if (end < start){
-        return 0;
-	}
-
-    for (uintptr_t addr = start; addr < end; addr += PAGING_PAGE_SIZE){
-        if (!mmu_user_pointer_valid((void*)addr)){
-            return 0;
+	struct mem_region* mem_region = NULL;
+	if(mm){
+		mem_region = vma_region_create(physaddr, virtaddr, size, flags, MAP_ANONYMOUS);
+		if(!mem_region){
+			return NO_MEMORY;
 		}
-    }
-
-    return 1;
-}
-
-void* phys_to_virt(void* physicalAddr){
-	return (void*)((uintptr_t)physicalAddr + KERNEL_VIRT_BASE - KERNEL_PHYS_BASE);
-}
-
-void mmu_copy_kernel_to_directory(struct PagingDirectory* directory){
-	for (uint16_t i = (KERNEL_VIRT_BASE >> 22); i < 1023; i++){
-		directory->entry[i] = _kernelDirectory->entry[i];
 	}
 
-	// ensurence self PDE
-	directory->entry[SELF_PDE_INDEX] = (PagingTable)((uintptr_t)mmu_translate(directory->entry) | FPAGING_P | FPAGING_RW);
+	for(unsigned int i = 0; i < count; i++){
+		int status = pgd_map(paddr, vaddr, flags);
+		if(status != SUCCESS){
+			vaddr = (uintptr_t)virtaddr;
+			for(unsigned int j = 0; j < i; j++){
+				if(pgd_unmap(vaddr) != SUCCESS)
+					warning("mmu_mmap(): failed to rollback map!\n");
+
+				vaddr += PTE_PAGE_SIZE;
+			}
+
+			kfree(mem_region);
+			return status;
+		}
+
+		vaddr += PTE_PAGE_SIZE;
+		paddr += PTE_PAGE_SIZE;
+	}
+
+	if(mm) vma_add(mm, mem_region);
+
+	return SUCCESS;
+}
+
+int mmu_munmap(struct mm_struct* mm, void* virtaddr, int size){
+	if(!virtaddr || size <= 0){
+		return INVALID_ARG;
+	}
+
+	uintptr_t vaddr = (uintptr_t)virtaddr;
+
+	struct mem_region* mem_region = NULL;
+
+	if(mm){
+		mem_region = vma_lookup(mm, virtaddr);
+		if(IS_ERR(mem_region)){
+			return PTR_ERR(mem_region);
+		}
+	}
+
+	unsigned int alignedSize = (size + PTE_PAGE_SIZE - 1) & ~(PTE_PAGE_SIZE - 1);
+	unsigned int count = alignedSize / PTE_PAGE_SIZE;
+
+	for(unsigned int i = 0; i < count; i++){
+		int status = pgd_unmap(vaddr);
+		if(status != SUCCESS){
+			return status;
+		}
+
+		vaddr += PTE_PAGE_SIZE;
+	}
+
+	if(mm){
+		// TODO: implement resize mem size if possible
+
+		vma_remove(mm, mem_region);
+	}
+
+	return SUCCESS;
+}
+
+int mmu_set_flags(struct mm_struct* mm, void* virtaddr, int size, mem_flags_t flags){
+	if(!virtaddr || size <= 0){
+		return INVALID_ARG;
+	}
+
+	uintptr_t vaddr = (uintptr_t)virtaddr;
+	mem_flags_t old_flags;
+
+	unsigned int alignedSize = (size + PTE_PAGE_SIZE - 1) & ~(PTE_PAGE_SIZE - 1);
+	unsigned int count = alignedSize / PTE_PAGE_SIZE;
+
+	struct mem_region* mem_region = NULL;
+
+	if(mm){
+		mem_region = vma_lookup(mm, virtaddr);
+		if(IS_ERR(mem_region)){
+			return PTR_ERR(mem_region);
+		}
+
+		old_flags = mem_region->mem_flags;
+	}else{
+		old_flags = pte_get_flags(vaddr);
+	}
+
+	for(unsigned int i = 0; i < count; i++){
+		int status = pte_update_flags(vaddr, flags);
+		if(status != SUCCESS){
+			vaddr = (uintptr_t)virtaddr;
+			for(unsigned int j = 0; j < i; j++){
+				if(pte_update_flags(vaddr, old_flags) != SUCCESS)
+					warning("mmu_set_flags(): failed to rollback flags!\n");
+
+				vaddr += PTE_PAGE_SIZE;
+			}
+
+			return status;
+		}
+
+		vaddr += PTE_PAGE_SIZE;
+	}
+
+	if(mm){
+		mem_region->mem_flags = flags;
+	}
+
+	return SUCCESS;
+}
+
+int mmu_page_switch(pgd_t* pgd){
+	return pgd_load(pgd);
+}
+
+int mmu_destroy_page(pgd_t* pgd){
+	int res = pgd_load(_kernel_pgd);
+	if(IS_STAT_ERR(res)){
+		return res;
+	}
+
+	pgd_remove_kernel(pgd);
+
+	pgd_free(pgd);
+
+	return SUCCESS;
+}
+
+pgd_t* mmu_create_page(){
+	pgd_t* pgd = pgd_alloc();
+	if(pgd){
+		pgd_copy_kernel(_kernel_pgd, pgd);
+	}
+
+	return pgd;
+}
+
+void* mmu_translate(void* virtaddr){
+	return pgd_translate(virtaddr);
 }
