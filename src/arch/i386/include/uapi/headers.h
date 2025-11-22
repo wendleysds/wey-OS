e10696d1046e6da3dbd6facf0dd2f794b6f4c219
Wendley Santos <wendley.sdsilva@gmail.com>
2025-11-22 20:37:31 -0300

    refractor: field members to help bootloader

--- src/arch/i386/include/uapi/headers.h
+++ src/arch/i386/include/uapi/headers.h
@@ -6,14 +6,18 @@
 
 struct setup_header{
 	uint8_t setup_sectors;
+	uint32_t syssize;
 	uint16_t video_mode;
+	uint16_t sig;
 	uint16_t root_device;
+	uint16_t code16_start;
 	uint32_t code32_start;
 	uint64_t pref_address;
 } __attribute__ ((packed));
 
 struct boot_header{
 	struct video_info video_info;
+	uint8_t pad[16 - (sizeof(struct video_info) % 16)]; // Align 16 bytes
 	struct setup_header setup_header;
 } __attribute__ ((packed));
 
