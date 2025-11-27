679fa58ae59911ddd8a98e9e4645ad3db84b2f81
Wendley Santos <wendley.sdsilva@gmail.com>
2025-11-27 17:50:20 -0300

    feat: ls

--- tools/fs/fat/commands.py
+++ tools/fs/fat/commands.py
@@ -1,13 +1,18 @@
 from fat import FATFS, FATDirectoryEntry
 import fat as attr
 
+ATTRIBUTES = {
+	'-hi': attr.HIDDEN,
+	'-vo': attr.VOLUME,
+	'-sy': attr.SYSTEM,
+	'-ro': attr.READY_ONLY
+}
+
 def cluster_to_lba(fat: FATFS, cluster: int) -> int:
     return fat.firstDataSector + ((cluster - 2) * fat.headers.boot.secPerClus)
 
 def get_cluster_entry(entry: FATDirectoryEntry) -> int:
     return (entry.fstClusHI << 16) | entry.fstClusLO
-
-def init(args: list[str], src: str, dst: str) -> int: pass
 
 def creat(fat: FATFS, args: list[str], src: str, dst: str) -> int:
 	tokens = src.rstrip('/').split('/')
@@ -19,13 +24,6 @@ def creat(fat: FATFS, args: list[str], src: str, dst: str) -> int:
 		return 1
 
 	cluster = get_cluster_entry(entry)
-
-	ATTRIBUTES = {
-		'-HI': attr.HIDDEN,
-		'-VO': attr.VOLUME,
-		'-SY': attr.SYSTEM,
-		'-RO': attr.READY_ONLY
-	}
 
 	attrs = 0
 	for arg in args:
@@ -45,7 +43,30 @@ def creat(fat: FATFS, args: list[str], src: str, dst: str) -> int:
 
 def mkdir(args: list[str], src: str, dst: str) -> int: pass
 def rm(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
-def ls(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
+
+def ls(fat: FATFS, args: list[str], src: str, dst: str) -> int:
+	entry = fat.walk(src)
+
+	if not entry:
+		print("No such file or directory")
+		return 1
+
+	if not entry.attr & attr.DIRECTORY:
+		print(src)
+		return
+
+	entries = fat.ls(get_cluster_entry(entry))
+
+	for e in entries:
+		name = e.name.decode().strip()
+		if e.attr & attr.DIRECTORY:
+			print(f"\033[1;34m{name}\033[0m/", end='')
+		else:
+			print(f"{name[:8].rstrip()}.{name[8:11].rstrip()}", end='')
+
+		print("   ", end='')
+	print('')
+
 def read(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
 def stat(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
 def statfs(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
