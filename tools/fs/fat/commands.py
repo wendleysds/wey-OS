a493d052bef3f1d9c3ff7be0315f9f45c8979862
Wendley Santos <wendley.sdsilva@gmail.com>
2025-11-28 00:23:43 -0300

    feat: stat command

--- tools/fs/fat/commands.py
+++ tools/fs/fat/commands.py
@@ -104,6 +104,17 @@ def ls(fat: FATFS, args: list[str], src: str, dst: str) -> int:
 	print('')
 
 def read(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
-def stat(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
+def stat(fat: FATFS, args: list[str], src: str, dst: str) -> int:
+	entry = fat.walk(src)
+	if not entry:
+		print("No such file or directory")
+		return 1
+
+	cluster = get_cluster_entry(entry)
+	lba = cluster_to_lba(fat, cluster)
+	off = (fat.fat.mbr.startLBA + lba) * fat.fat.header_boot.bytesPerSec
+	print(entry)
+	print(f"data LBA     :  {lba} - {off}:{hex(off)}")
+
 def statfs(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
 def cp(fat: FATFS, args: list[str], src: str, dst: str) -> int: pass
