aeab49602056d19fb7b505cb19d86219bac229ac
Wendley Santos <wendley.sdsilva@gmail.com>
2026-01-13 19:06:24 -0300

    refractor: kernel memory layout

--- src/include/def/config.h
+++ src/include/def/config.h
@@ -1,66 +1,110 @@
 #ifndef _GLOBAL_CONFIG_H
 #define _GLOBAL_CONFIG_H
 
-#define KERNEL_CODE_SELECTOR 0x08
-#define KERNEL_DATA_SELECTOR 0x10
+/*GDT*/
+#define TOTAL_GDT_SEGMENTS 6
 
-#define USER_CODE_SEGMENT 0x1b
-#define USER_DATA_SEGMENT 0x23
+#define GDT_NULL_INDEX        0 
+#define GDT_KERNEL_CODE_INDEX 1
+#define GDT_KERNEL_DATA_INDEX 2
+#define GDT_USER_CODE_INDEX   3
+#define GDT_USER_DATA_INDEX   4
+#define GDT_TSS_INDEX         5
+
+#define GDT_KERNEL_CODE  (GDT_KERNEL_CODE_INDEX << 3)
+#define GDT_KERNEL_DATA  (GDT_KERNEL_DATA_INDEX << 3)
+#define GDT_USER_CODE    ((GDT_USER_CODE_INDEX << 3) | 3)
+#define GDT_USER_DATA    ((GDT_USER_DATA_INDEX << 3) | 3)
+#define GDT_TSS          (GDT_TSS_INDEX << 3)
 
 #ifndef __ASSEMBLY__
+extern unsigned long __kernel_phys_base;
+extern unsigned long __kernel_high_base;
+
 extern unsigned long __kernel_phys_start;
+extern unsigned long __kernel_phys_end;
 extern unsigned long __kernel_high_start;
+extern unsigned long __kernel_high_end;
+#else
+extern __kernel_phys_base
+extern __kernel_high_base
+
+extern __kernel_phys_start
+extern __kernel_phys_end
+extern __kernel_high_start
+extern __kernel_high_end
 #endif
 
-#define KERNEL_PHYS_BASE ((unsigned long)&__kernel_phys_start)
-#define KERNEL_VIRT_BASE ((unsigned long)&__kernel_high_start)
+#ifndef __ASSEMBLY__
+#define KERNEL_PHYS_BASE  ((unsigned long)&__kernel_phys_base)
+#define KERNEL_VIRT_BASE  ((unsigned long)&__kernel_high_base)
 
-#define TOTAL_GDT_SEGMENTS 6
+#define KERNEL_PHYS_START ((unsigned long)&__kernel_phys_start)
+#define KERNEL_PHYS_END   ((unsigned long)&__kernel_phys_end)
+#define KERNEL_VIRT_START ((unsigned long)&__kernel_high_start)
+#define KERNEL_VIRT_END   ((unsigned long)&__kernel_high_end)
+#else
+#define KERNEL_PHYS_BASE  [__kernel_phys_base]
+#define KERNEL_VIRT_BASE  [__kernel_high_base]
+
+#define KERNEL_PHYS_START [__kernel_phys_start]
+#define KERNEL_PHYS_END   [__kernel_phys_end]
+#define KERNEL_VIRT_START [__kernel_high_start]
+#define KERNEL_VIRT_END   [__kernel_high_end]
+#endif
+
 #define TOTAL_INTERRUPTS 256
 #define TIMER_FREQUENCY 20
-
-/* Memory */
-
-/* Heap Memory Layout
-
-	-------
-	|     | heap bitmap(meta data) <- HEAP_PHYS
-	|     | heap start <- HEAP_PHYS + bitmap size = (HEAP_SIZE_BYTES / HEAP_BLOCK_SIZE)
-	|     | heap end <- heap star + HEAP_SIZE_BYTES
-	-------
-/*/
 
 #define KiB(x) (x * 1024)
 #define MiB(x) (x * KiB(1024))
 
-#define _TEMP_PAGE_DIRECTORY_ADDRESS 0x02000000
-
-#define HEAP_SIZE_BYTES MiB(40)
-#define HEAP_BLOCK_SIZE 4096
-
 #define ALIGN(value, alignment) (((value) + (alignment) - 1) & ~((alignment) - 1))
 
-#define HEAP_PHYS 0x01000000
-#define HEAP_VIRT 0xC2000000
+/* Memory Layout
 
-#define HEAP_TABLE_PHYS_BASE (HEAP_PHYS)
-#define HEAP_TABLE_PHYS_END  (HEAP_TABLE_PHYS_BASE + (HEAP_SIZE_BYTES / HEAP_BLOCK_SIZE))
+0x00000000  +----------------------------------------+
+            | User Space                             |
+0xC0000000  +----------------------------------------+ 0x100000
+            | Kernel image (.text/.data/.bss)        |
+            |                                        |
+            | __kernel_high_start                    |
+            | __kernel_high_end                      |
+            +----------------------------------------+
+            | Early allocator / init sections        |
+			| Early Pages & Tables                   |
+            | Page metadata (struct page[])          |
+            +----------------------------------------+
+            | Kernel stack + page guard (Per-CPU)    |
+            +----------------------------------------+
+            | Slab / kmalloc area (lowmem)           |
+0xEFFFFFFF  +----------------------------------------+
+            | NON-LINEAR / MMIO                      | ~256MiB
+0xFFFFFFFF  +----------------------------------------+
 
-#define HEAP_TABLE_VIRT_BASE (HEAP_VIRT)
-#define HEAP_TABLE_VIRT_END (HEAP_TABLE_VIRT_BASE + (HEAP_SIZE_BYTES / HEAP_BLOCK_SIZE))
+*/
 
-#define HEAP_PHYS_BASE (ALIGN(HEAP_TABLE_PHYS_END, HEAP_BLOCK_SIZE))
-#define HEAP_PHYS_END  (HEAP_PHYS_BASE + HEAP_SIZE_BYTES)
+#define PAGE_SIZE 0x1000
 
-#define HEAP_VIRT_BASE (ALIGN(HEAP_TABLE_VIRT_END, HEAP_BLOCK_SIZE))
-#define HEAP_VIRT_END  (HEAP_VIRT_BASE + HEAP_SIZE_BYTES)
+#define RESERVED_SIZE       MiB(8)
+#define KERNEL_STACKS_SIZE  MiB(1)
 
-#define KERNEL_STACK_SIZE KiB(512)
-#define KERNEL_STACK_PHYS_TOP 0x00200000
-#define KERNEL_STACK_PHYS_BOTTOM (KERNEL_STACK_PHYS_TOP - KERNEL_STACK_SIZE)
+#define USER_SPACE_START    0x00000000UL
+#define USER_SPACE_END      0xBFFFFFFFUL
 
-#define KERNEL_STACK_VIRT_TOP 0xC1000000
-#define KERNEL_STACK_VIRT_BOTTOM (KERNEL_STACK_VIRT_TOP - KERNEL_STACK_SIZE)
+#define KERNEL_MMIO_START   0xF0000000UL
+#define KERNEL_MMIO_END     0xFFFFFFFFUL
+
+#define KERNEL_EARLY_START  (ALIGN(KERNEL_VIRT_END, PAGE_SIZE))
+#define KERNEL_EARLY_END    (KERNEL_EARLY_START + RESERVED_SIZE - 1)
+
+#define KERNEL_STACKS_START  (KERNEL_EARLY_END + 1)
+#define KERNEL_STACKS_END    (KERNEL_STACKS_START + KERNEL_STACKS_SIZE - 1)
+
+#define KERNEL_HEAP_START   (ALIGN(KERNEL_STACKS_END + 1, PAGE_SIZE))
+#define KERNEL_HEAP_END     (KERNEL_MMIO_START - 1)
+
+#define KERNEL_MMIO_SIZE    (KERNEL_MMIO_END - KERNEL_MMIO_START)
 
 #define EARLY_STACK_SIZE KiB(4)
 #define EARY_STACK_ADDR_BOTTOM 0x90000
@@ -87,10 +131,8 @@ extern unsigned long __kernel_high_start;
 #define PROC_USER_STACK_SIZE 8192
 #define PROC_USER_STACK_VIRUTAL_BUTTOM (PROC_USER_STACK_VIRUTAL_TOP - PROC_USER_STACK_SIZE)
 
-#define PROC_KERNEL_STACK_VIRTUAL_TOP 0xC1100000
-#define PROC_KERNEL_STACK_SIZE 4096
-#define PROC_KERNEL_STACK_VIRTUAL_BUTTOM (PROC_KERNEL_STACK_VIRTUAL_TOP - PROC_KERNEL_STACK_SIZE)
-
-#define PROC_VIRTUAL_ADDRESS 0x400000
+/*Terminal/Console*/
+#define TERMINALS_MAX 12
+#define VIDEO_DRIVERS_MAX 4
 
 #endif
