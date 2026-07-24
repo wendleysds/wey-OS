f1f8469af02bc09a69f40c7cc7ba2a8be1e45d2f
Wendley Santos <wendley.sdsilva@gmail.com>
2026-07-23 22:14:17 -0300

    feat(keycodes): Added more keycodes

--- src/include/kernel/input/keycodes.h
+++ src/include/kernel/input/keycodes.h
@@ -7,76 +7,87 @@ enum input_keycode {
 
 	KEY_ESC,
 
-	KEY_1,
-	KEY_2,
-	KEY_3,
-	KEY_4,
-	KEY_5,
-	KEY_6,
-	KEY_7,
-	KEY_8,
-	KEY_9,
-	KEY_0,
+	// Numbers
+	KEY_0, KEY_1, KEY_2, KEY_3, KEY_4,
+	KEY_5, KEY_6, KEY_7, KEY_8, KEY_9,
 
-	KEY_A,
-	KEY_B,
-	KEY_C,
-	KEY_D,
-	KEY_E,
-	KEY_F,
-	KEY_G,
-	KEY_H,
-	KEY_I,
-	KEY_J,
-	KEY_K,
-	KEY_L,
-	KEY_M,
-	KEY_N,
-	KEY_O,
-	KEY_P,
-	KEY_Q,
-	KEY_R,
-	KEY_S,
-	KEY_T,
-	KEY_U,
-	KEY_V,
-	KEY_W,
-	KEY_X,
-	KEY_Y,
-	KEY_Z,
+	// Alphabet
+	KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F, KEY_G, KEY_H,
+	KEY_I, KEY_J, KEY_K, KEY_L, KEY_M, KEY_N, KEY_O, KEY_P,
+	KEY_Q, KEY_R, KEY_S, KEY_T, KEY_U, KEY_V, KEY_W, KEY_X,
+	KEY_Y, KEY_Z,
 
-	KEY_F1,
-	KEY_F2,
-	KEY_F3,
-	KEY_F4,
-	KEY_F5,
-	KEY_F6,
-	KEY_F7,
-	KEY_F8,
-	KEY_F9,
-	KEY_F10,
-	KEY_F11,
-	KEY_F12,
+	// Functions
+	KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6,
+	KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12,
+	KEY_F13, KEY_F14, KEY_F15, KEY_F16, KEY_F17, KEY_F18,
+	KEY_F19, KEY_F20, KEY_F21, KEY_F22, KEY_F23, KEY_F24,
 
+	// Control
+	KEY_TAB,
 	KEY_ENTER,
 	KEY_SPACE,
 	KEY_BACKSPACE,
-	KEY_TAB,
+
+	// Modifiers
 	KEY_CAPSLOCK,
 	KEY_NUMLOCK,
 	KEY_SCROLLLOCK,
-
-	KEY_LEFT,
-	KEY_RIGHT,
-	KEY_UP,
-	KEY_DOWN,
-
 	KEY_LEFT_SHIFT,
 	KEY_RIGHT_SHIFT,
 	KEY_LEFT_CTRL,
 	KEY_RIGHT_CTRL,
 	KEY_LEFT_ALT,
 	KEY_RIGHT_ALT,
+	KEY_LEFT_GUI,   // Win/Super Left
+	KEY_RIGHT_GUI,  // Win/Super Right
+	KEY_MENU,
+
+	// Punctuation / Symbols
+	KEY_LEFT_BRACE,
+	KEY_RIGHT_BRACE,
+	KEY_MINUS,
+	KEY_EQUAL,
+	KEY_SEMI_COLON,
+	KEY_APOSTROPHE,
+	KEY_GRAVE,
+	KEY_BACK_SLASH,
+	KEY_COMMA,
+	KEY_DOT,
+	KEY_SLASH,
+	KEY_QUOTE,
+
+	// Dedicated Navigation Cluster
+	KEY_INSERT,
+	KEY_DELETE,
+	KEY_HOME,
+	KEY_END,
+	KEY_PAGE_UP,
+	KEY_PAGE_DOWN,
+	KEY_UP,
+	KEY_DOWN,
+	KEY_LEFT,
+	KEY_RIGHT,
+
+	// Number Pad
+	KEY_KP_0, KEY_KP_1, KEY_KP_2, KEY_KP_3, KEY_KP_4,
+	KEY_KP_5, KEY_KP_6, KEY_KP_7, KEY_KP_8, KEY_KP_9,
+	KEY_KP_DOT,
+	KEY_KP_ASTERISK,
+	KEY_KP_MINUS,
+	KEY_KP_PLUS,
+	KEY_KP_SLASH,
+	KEY_KP_ENTER,
+
+	// System / Special
+	KEY_PRINT_SCREEN,
+	KEY_PAUSE,
+
+	// International / ABNT2 specific
+	KEY_ABNT_C1, // Key /? in ABNT2
+	KEY_ABNT_C2, // Key . in Numpad ABNT2
+
+	NR_KEYS
 };
 
 #endif
